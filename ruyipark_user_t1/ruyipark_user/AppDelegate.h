74400f68bf02e9948ca1ab0f063a2498122f5e34
lanzi0907 <wilsonlee8387@gmail.com>
2014-03-15 15:57:22 +0800

    Add sub-folder to test

---
+++ ruyipark_user_t1/ruyipark_user/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ruyipark_user
+//
+//  Created by 李瑜伟 on 14-3-10.
+//  Copyright (c) 2014年 ruyipark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ruyipark_user_t1/ruyipark_user/MapAPIKey.h
@@ -0,0 +1,19 @@
+//
+//  MapAPIKey.h
+//  ruyipark_user
+//
+//  Created by 李瑜伟 on 14-3-11.
+//  Copyright (c) 2014年 ruyipark. All rights reserved.
+//
+
+#ifndef ruyipark_user_MapAPIKey_h
+#define ruyipark_user_MapAPIKey_h
+
+/* 设置高德地图API key
+ */
+
+const static NSString *APIKey = @"dc24ccedb4f866dbdc353fefa46eda5a";
+
+
+
+#endif
